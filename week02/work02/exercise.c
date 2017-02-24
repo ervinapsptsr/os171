005518815d265a5e2930e0a80309d11bbd839130
Ervina Puspita Sari - 1506689414 <ervina.puspita@badak.cs.ui.ac.id>
2017-02-25 00:15:59 +0700

    Week 2 already

---
+++ week02/work02/exercise.c
@@ -0,0 +1,27 @@
+/* 
+ * Name: Ervina Puspita Sari
+ * NPM: 1506689414
+ * Class: OS A 
+ * Comment: Hellooo there
+ */ 
+ 
+#define LOOP 5 
+#include <stdio.h>
+
+void main()
+{
+	int input = 4; 
+	int ii;
+	int product =0;
+	for (ii=0; ii<LOOP; ii++) {
+      product += input;
+      
+	}
+	printf("%d",input);
+	printf(" times ");
+	printf("%d",LOOP);
+	printf(" equals ");
+	printf("%d",product);
+ 
+}
+
